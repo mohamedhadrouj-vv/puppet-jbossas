f694c5fc6db5a6eae3f8add5611e167d32ebb19c
mohamedhadrouj-vv <mohamed.hadrouj@viavoo.com>
2013-02-22 16:05:51 +0100

    Added: cleaning up jboss from jsf libs + added argument to specify desired JSF version

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,6 +24,7 @@ define jbossas::server (
   $download_dir = '/tmp/jboss',
   $jboss_home = '/home/jboss',
   $jboss_dirname = 'jboss',
+  $jsf_api_version = "1.2_15",
 ){
 
     #Download and install Jboss
@@ -43,6 +44,45 @@ define jbossas::server (
            "${jboss_home}/${jboss_dirname}/lib/endorsed/jaxb-api.jar"] :
       ensure   => absent,
       require  => Jbossas::Install["${name}"]
+    }
+
+    #Download JSF api files
+    $local_jsf_dir = "${jboss_home}/${jboss_dirname}/server/default/deploy/jboss-web.deployer/jsf-libs"
+    $jsf_api_url = "http://repo1.maven.org/maven2/javax/faces/jsf-api/${jsf_api_version}/jsf-api-${jsf_api_version}.jar"
+    $jsf_api_impl_url = "http://repo1.maven.org/maven2/javax/faces/jsf-impl/${jsf_api_version}/jsf-impl-${jsf_api_version}.jar"
+
+    #Download the JSF libs
+    exec { "jsf_api_${name}":
+      command   => "curl --progress-bar -O ${jsf_api_url} -L",
+      cwd       => "${local_jsf_dir}",
+      path      => "/usr/bin/",
+      user      => $user,
+      logoutput => true,
+      timeout   => 0,
+      unless    => "test -f jsf-api-${jsf_api_version}.jar",
+      require   => [ Package['curl'] ],
+    }
+
+    exec { "jsf_api_impl_${name}":
+      command   => "curl --progress-bar -O ${jsf_api_impl_url} -L",
+      cwd       => "${local_jsf_dir}",
+      path      => "/usr/bin/",
+      user      => $user,
+      logoutput => true,
+      timeout   => 0,
+      unless    => "test -f jsf-impl-${jsf_api_version}.jar",
+      require   => [ Package['curl'] ],
+    }
+
+    file { ["${local_jsf_dir}/jboss-faces.jar",
+            "${local_jsf_dir}/jsf-api-${jsf_api_version}.jar",
+            "${local_jsf_dir}/jsf-impl-${jsf_api_version}.jar"]:
+      ensure  => present,
+      purge   => true,
+      recurse => true,
+      owner   => $user,
+      group   => $group,
+      require => [ Exec["jsf_api_impl_${name}"], Exec["jsf_api_${name}"] ],
     }
 
 }
